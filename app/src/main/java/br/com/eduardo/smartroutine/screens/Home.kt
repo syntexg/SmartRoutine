b77b8f8c3ae0283cd00cf1b563366e993b6cf379
Eduardo <edmaciel.03@gmail.com>
2024-04-23 22:01:35 -0300

    update

---
+++ app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
@@ -0,0 +1,14 @@
+package br.com.eduardo.smartroutine.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Home(){
+    // A surface container using the 'background' color from the theme
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+    }
+}
