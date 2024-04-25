27e821e03f33e931fa28c87e716285eaeb889cf6
joaosteinn <vitinhostein@gmail.com>
2024-04-25 20:12:36 -0300

    update

--- app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
+++ app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
@@ -1,14 +1,28 @@
 package br.com.eduardo.smartroutine.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import br.com.eduardo.smartroutine.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(){
     // A surface container using the 'background' color from the theme
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(stringResource(R.string.title_login_screen))
+                    }
+
+                )
+            }
+        ) {
+
+        }
     }
 }
