41bbc644c7d62824006102b949209c8dc5a7beed
joaosteinn <vitinhostein@gmail.com>
2024-04-26 14:13:31 -0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,14 +48,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
+++ app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
-import androidx.compose.material3.R
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +12,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import br.com.eduardo.smartroutine.R
+
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,14 +35,27 @@ fun Home() {
                         ) {
                             Image(
                                 painter = painterResource(id = R.drawable.logo_2_removebg_preview),
-                                contentDescription = "App TopBar Logo",
+                                contentDescription = null,
                                 modifier = Modifier.scale(0.8f)
                             )
                         }
                     }
                 )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = {
+                    }
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icon),
+                        contentDescription = null,
+                        modifier = Modifier.scale(3f)
+                    )
+                }
             }
-        ){
+        ) {
+
         }
     }
 }
