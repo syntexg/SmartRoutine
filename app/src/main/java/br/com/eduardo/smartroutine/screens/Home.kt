1a3b503b9b5abd4434c01fea82fa5aa19e850486
joaosteinn <vitinhostein@gmail.com>
2024-04-26 13:08:45 -0300

    update

--- app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
+++ app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
@@ -1,32 +1,30 @@
 package br.com.eduardo.smartroutine.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
+import androidx.compose.material3.R
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import br.com.eduardo.smartroutine.R
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
-    // A surface container using the 'background' color from the theme
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
         Scaffold(
             topBar = {
                 TopAppBar(
                     colors = TopAppBarDefaults.topAppBarColors(
                         containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary
+                        titleContentColor = MaterialTheme.colorScheme.primary,
                     ),
                     title = {
                         Box(
@@ -35,35 +33,14 @@ fun Home() {
                         ) {
                             Image(
                                 painter = painterResource(id = R.drawable.logo_2_removebg_preview),
-                                contentDescription = null,
+                                contentDescription = "App TopBar Logo",
                                 modifier = Modifier.scale(0.8f)
                             )
                         }
                     }
                 )
-            },
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = {
-                    }
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icon),
-                        contentDescription = null,
-                        modifier = Modifier.scale(3f)
-                    )
-                }
             }
-        ) {
-            paddingValues ->  
-            Column(
-                Modifier.fillMaxSize().padding(
-                    vertical = paddingValues.calculateTopPadding(),
-                    horizontal = 16.dp
-                )
-            ){
-
-            }
+        ){
         }
     }
 }
