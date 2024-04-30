a36cb319406f6e20050ed6d3ecc0c7d804f909cd
joaosteinn <vitinhostein@gmail.com>
2024-04-30 20:19:28 -0300

    BottomBar adicionada e ícone de usuário.

--- app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
+++ app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
@@ -13,49 +13,91 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import br.com.eduardo.smartroutine.R
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
 
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                    title = {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.logo_2_removebg_preview),
-                                contentDescription = null,
-                                modifier = Modifier.scale(0.8f)
-                            )
-                        }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_2_removebg_preview),
+                            contentDescription = null,
+                            modifier = Modifier.scale(0.8f)
+                        )
                     }
-                )
-            },
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = {
-                    }
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icon),
-                        contentDescription = null,
-                        modifier = Modifier.scale(3f)
-                    )
                 }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.icon),
+                    contentDescription = "",
+                    modifier = Modifier.scale(3f)
+                )
             }
-        ) {
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primaryContainer
+            ) {
 
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            Modifier.fillMaxSize().padding(
+                vertical = paddingValues.calculateTopPadding(),
+                horizontal = 16.dp
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                Icons.Default.AccountCircle,
+                contentDescription = "Account icon",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(96.dp),
+            )
         }
     }
 }
+
+data class BottomNavItem(
+    val icon: ImageVector,
+    val label: String,
+    val selected: Boolean,
+    val onClick: () -> Unit
+)
