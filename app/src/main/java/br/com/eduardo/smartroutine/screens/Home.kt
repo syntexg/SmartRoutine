f758ad5d62c881c3d5133d7f045399dd76971abc
Eduardo <edmaciel.03@gmail.com>
2024-05-02 20:29:39 -0300

    update

--- app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
+++ app/src/main/java/br/com/eduardo/smartroutine/screens/Home.kt
@@ -9,17 +9,18 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import br.com.eduardo.smartroutine.R
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
     var selectedItem by remember { mutableStateOf(0) }
+    val selectedColor = Color(android.graphics.Color.parseColor("#3D1365"))
     Scaffold(
         topBar = {
             TopAppBar(
@@ -46,36 +47,89 @@ fun Home() {
                 onClick = {
                 }
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.icon),
+                Icon(
+                    Icons.Default.Add,
                     contentDescription = "Floating button icon",
-                    modifier = Modifier.scale(3f)
                 )
             }
         },
         bottomBar = {
             BottomAppBar(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primaryContainer
+                contentColor = MaterialTheme.colorScheme.primary
             ) {
                 NavigationBar(
                     containerColor = MaterialTheme.colorScheme.primaryContainer
                 ) {
                     NavigationBarItem(
                         modifier = Modifier.scale(1.3f),
-                        icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-                        label = { Text("Home") },
+                        icon = {
+                            if (selectedItem == 0) {
+                                Icon(
+                                    Icons.Default.Home,
+                                    contentDescription = "Home",
+                                    tint = selectedColor
+                                )
+                            } else {
+                                Icon(Icons.Default.Home, contentDescription = "Home")
+                            }
+                        },
+                        label = {
+                            if (selectedItem == 0) {
+                                Text("Home", color = selectedColor)
+                            } else {
+                                Text("Home")
+                            }
+                        },
                         selected = selectedItem == 0,
                         onClick = { selectedItem = 0 }
                     )
                     NavigationBarItem(
                         modifier = Modifier.scale(1.3f),
-                        icon = { Icon(Icons.Default.AccountCircle, contentDescription = "Profile") },
-                        label = { Text("Profile") },
+                        icon = {
+                            if (selectedItem == 1) {
+                                Icon(
+                                    Icons.Default.Done,
+                                    contentDescription = "Done",
+                                    tint = selectedColor
+                                )
+                            } else {
+                                Icon(Icons.Default.Done, contentDescription = "Profile")
+                            }
+                        },
+                        label = {
+                            if (selectedItem == 1) {
+                                Text("Done", color = selectedColor)
+                            } else {
+                                Text("Done")
+                            }
+                        },
                         selected = selectedItem == 1,
                         onClick = { selectedItem = 1 }
                     )
-                }
+                    NavigationBarItem(
+                        modifier = Modifier.scale(1.3f),
+                        icon = {
+                            if (selectedItem == 2) {
+                                Icon(
+                                    Icons.Default.Delete,
+                                    contentDescription = "Trash",
+                                    tint = selectedColor
+                                )
+                            } else {
+                                Icon(Icons.Default.Delete, contentDescription = "Delete")
+                            }
+                        },
+                        label = {
+                            if (selectedItem == 2) {
+                                Text("Delete", color = selectedColor)
+                            } else {
+                                Text("Delete")
+                            }
+                        },
+                        selected = selectedItem == 2,
+                        onClick = { selectedItem = 2 }
+                    ) }
             }
         }
     ) { paddingValues ->
